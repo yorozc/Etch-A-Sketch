bf5265e79a2344d55cdaa04fb67019ddb0509ea9
Mick <yorozclop49@gmail.com>
2024-05-02 20:25:35 -0700

    added event listener when hovering over divs

--- etch.js
+++ etch.js
@@ -1,16 +1,26 @@
 const container = document.getElementById("grid_container")
-container.style.border = '1px solid black'
-container.style.justifyContent = 'space-between'
+container.style.flexWrap = 'wrap'
 
 
 for (let i = 0; i < 16; i++){
-    const box = document.createElement("div")
-    box.classList.add("box")
-    box.textContent = 'box ' + (i+1)
-    box.style.border = '2px solid purple'
-    box.style.flexGrow = 1
-    box.style.height = '50px'
-    container.appendChild(box)
+    for(let j = 0; j < 17; j++){
+        const box = document.createElement("div")
+        box.classList.add("boxes")
+        box.style.border = '1px solid black'
+        box.style.flexGrow = 1
+        box.style.height = '50px'
+        box.style.width = '50px'
+        container.appendChild(box)
 
+        box.addEventListener("mouseover", () =>{
+            box.style.backgroundColor = "black"
+        })
+        
+
+
+        
+    }
 }
+
+
 
