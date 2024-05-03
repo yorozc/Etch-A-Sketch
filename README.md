9f66e47b9f8b47f6dde771dbaae76e6e692583c2
Mick <yorozclop49@gmail.com>
2024-05-02 17:44:45 -0700

    added the first row of the etch and sketch

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Etch-A-Sketch
+- This program will simulate an etch and sketch
+- if you are unsure what this item is, it is explained in the link
+    - https://en.wikipedia.org/wiki/Etch_A_Sketch
--- etch.js
+++ etch.js
@@ -1,11 +1,16 @@
-const container = document.querySelector("#grid_container")
+const container = document.getElementById("grid_container")
 container.style.border = '1px solid black'
+container.style.justifyContent = 'space-between'
 
-const square_content = document.createElement("div") //this will be the square
-//figure out how to change width and height using div
-square_content.classList.add("square")
-square_content.textContent = "Hello"
-square_content.style.height = '250px'
-square_content.style.border = '1px solid red' 
 
-container.appendChild(square_content)
+for (let i = 0; i < 16; i++){
+    const box = document.createElement("div")
+    box.classList.add("box")
+    box.textContent = 'box ' + (i+1)
+    box.style.border = '2px solid purple'
+    box.style.flexGrow = 1
+    box.style.height = '50px'
+    container.appendChild(box)
+
+}
+
